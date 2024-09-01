19138675494e24a82b7df476b0d94b65f35de58c
Loïc Verilhac <loic.verilhac7@gmail.com>
2024-09-01 08:48:30 +0000

    Add another solution for problem 2022

---
+++ easy/2022 - Convert 1D Array Into 2D Array/typescript/solution2.ts
@@ -0,0 +1,11 @@
+function construct2DArray(original: number[], m: number, n: number): number[][] {
+    if (m*n !== original.length) return [];
+
+    const res = [];
+    for (let row = 0; row < m;row++) {
+        res.push(original.slice(row*n,row*n+n));
+        
+    }
+
+    return res;
+};
